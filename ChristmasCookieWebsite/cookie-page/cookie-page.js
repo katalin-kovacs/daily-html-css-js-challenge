17bb51fbc537986b248e8f9a3f320debfaac05e4
Katalin Kovács <kovacs.katalin89@gmail.com>
2023-12-19 15:27:43 +0100

    initial commit

---
+++ ChristmasCookieWebsite/cookie-page/cookie-page.js
@@ -0,0 +1,17 @@
+// Enter your javascript here
+function openTab(event, contentName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(contentName).style.display = "block";
+  event.currentTarget.className += " active";
+}
